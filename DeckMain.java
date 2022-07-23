ce8ae689cbb33873999593619ff7a19d16c3421d
vikashc332 <85497111+vikashc332@users.noreply.github.com>
2022-07-23 11:51:16 +0530

    Create DeckMain.java

---
+++ DeckMain.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Card card = new Card();
+        card.shuffleCard();
+        card.assignCards();
+        card.display();
+    }
+}
